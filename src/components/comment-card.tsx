c287c9bbf64422370a382bd24470b8bff3cdbb2e
LordWeedlle <virgile@doctoome.com>
2025-05-24 12:21:41 +0200

    refactor: add comment card component

---
+++ src/components/comment-card.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+import { format } from 'timeago.js'
+import { Box, Flex, Text } from '@chakra-ui/react'
+import { CommentResponse, GetUserByIdResponse } from '../api'
+import { AuthorAvatar } from '../components/author/avatar'
+import { AuthorName } from '../components/author/name'
+import { useAuthToken } from '../contexts/authentication'
+import { useAuthorCache } from '../contexts/author'
+
+export type CommentCardProps = {
+  comment: CommentResponse
+  memeId: string,
+}
+
+export const CommentCard: React.FC<CommentCardProps> = ({ comment, memeId }) => {
+  const token               = useAuthToken()
+  const { getAuthor }       = useAuthorCache()
+  const [author, setAuthor] = useState<GetUserByIdResponse | null>(null)
+
+  useEffect(() => {
+    let mounted = true
+
+    getAuthor(comment.authorId, token).then((data) => {
+      if (mounted) {
+        setAuthor(data)
+      }
+    })
+
+    return () => {
+      mounted = false
+    }
+  }, [comment.authorId, token, getAuthor])
+
+  return (
+    <Flex key={ comment.id }>
+      <AuthorAvatar author={ author } />
+      <Box p={ 2 } borderRadius={ 8 } bg="gray.50" flexGrow={ 1 }>
+        <Flex
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Flex>
+            <Text data-testid={ `meme-comment-author-${ memeId }-${ comment.id }` }>
+              <AuthorName author={ author } />
+            </Text>
+          </Flex>
+          <Text
+            fontStyle="italic"
+            color="gray.500"
+            fontSize="small"
+          >
+            { format(comment.createdAt) }
+          </Text>
+        </Flex>
+        <Text
+          color="gray.500"
+          whiteSpace="pre-line"
+          data-testid={ `meme-comment-content-${ memeId }-${ comment.id }` }
+        >
+          { comment.content }
+        </Text>
+      </Box>
+    </Flex>
+  )
+}
